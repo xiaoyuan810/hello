ccdf5c94fafba0276295341228c8163895613ead
xiaoyuan810 <963706922@qq.com>
2018-07-05 16:09:36 +0800

    提交

---
+++ src/EchoHello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hello;
+
+class EchoHello
+{
+    public static function say()
+    {
+        echo 'hello';
+    }
+}
---
+++ text.php
@@ -0,0 +1,7 @@
+<?php
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+use Hello\EchoHello;
+
+echo EchoHello::say();
